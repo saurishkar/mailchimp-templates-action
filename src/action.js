96f594a617f9c04a1053967dafe36136f0b5544e
Saurish Kar <saurish@snackmagic.com>
2022-08-28 02:08:18 +0530

    added mandrill template update function

--- src/action.js
+++ src/action.js
@@ -1,11 +1,28 @@
 const mailchimp = require("@mailchimp/mailchimp_transactional");
 const core = require("@actions/core");
 
+let mandrillClient;
+
+const updateTemplates = async () => {
+  const name = core.getInput("template_name");
+  const content = core.getInput("template_content");
+  const response = await mandrillClient.templates.update({
+    name,
+    code: content,
+    publish: true,
+  });
+  return response;
+};
+
 async function callPing() {
   const mandrillKey = core.getInput("mandrill_key");
-  const mandrillClient = mailchimp(mandrillKey);
+  mandrillClient = mailchimp(mandrillKey);
   const response = await mandrillClient.users.ping();
+  console.log("----------------------");
   console.log(response);
+  console.log("----------------------");
+  const updateResponse = await updateTemplates();
+  console.log(updateResponse);
 }
 
-callPing();
+callPing(); // if mandrill works, it will return "PONG!"
