575d6735f00fccf7fb4401cdd2d74344d792939f
Saurish Kar <saurish@snackmagic.com>
2022-08-28 01:26:53 +0530

    Added mandrill key as input to github action

--- src/action.js
+++ src/action.js
@@ -1,7 +1,10 @@
-const mailchimp = require("@mailchimp/mailchimp_transactional")("YOUR_API_KEY");
+const mailchimp = require("@mailchimp/mailchimp_transactional");
+const core = require("@actions/core");
 
 async function callPing() {
-  const response = await mailchimp.users.ping();
+  const mandrillKey = core.getInput("mandrill_key");
+  const mandrillClient = mailchimp(mandrillKey);
+  const response = await mandrillClient.users.ping();
   console.log(response);
 }
 
