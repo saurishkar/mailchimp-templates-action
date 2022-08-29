cff4e00aa540bad43a028005b9ca0351b2c6d295
Saurish Kar <saurish@snackmagic.com>
2022-08-29 14:43:12 +0530

    added support to get multiple files updated

--- src/action.js
+++ src/action.js
@@ -1,5 +1,7 @@
 const mailchimp = require("@mailchimp/mailchimp_transactional");
 const core = require("@actions/core");
+const fs = require("fs");
+const path = require("path");
 
 let mandrillClient;
 const mandrillKey = core.getInput("mandrill_key");
@@ -8,42 +10,79 @@ const templateCode = core.getInput("template_content");
 const templateSubject = core.getInput("template_subject");
 
 const validatePrimaryInputs = () => {
-  if(!templateName) {
+  if (!templateName) {
     throw Error("Template Name is Required");
   }
-  if(!templateCode) {
+  if (!templateCode) {
     throw Error("Template Code cannot be blank");
   }
-}
+};
 
-const addAndPublishTemplate = async() => {
-  if(!templateSubject) {
-    throw Error("Subject is required for adding a new template");
-  }
+const addAndPublishTemplate = async ({ name, content }) => {
   const response = await mandrillClient.templates.add({
-    name: templateName,
-    code: templateCode,
-    publish: true,
-    subject: templateSubject
-  });
-  return response;
-}
-
-const updateAndPublishTemplate = async () => {
-  const response = await mandrillClient.templates.update({
-    name: templateName,
-    code: templateCode,
+    name,
+    code: content,
     publish: true,
   });
   return response;
 };
 
-const findTemplate = async() => {
-  const response = await mandrillClient.templates.info({
-    name: templateName,
+const updateAndPublishTemplate = async ({ name, content }) => {
+  const response = await mandrillClient.templates.update({
+    name,
+    code: content,
+    publish: true,
   });
   return response;
-}
+};
+
+const findTemplate = async ({ name }) => {
+  const response = await mandrillClient.templates.info({
+    name,
+  });
+  return response;
+};
+
+const addOrUpdateTemplate = async ({ name, content }) => {
+  console.log("----------------------");
+  console.log("Check for template");
+  console.log("----------------------");
+  console.log("\n");
+  const { status: getTemplateStatus } = findTemplate({ name });
+
+  if (getTemplateStatus === 200) {
+    // Template Found
+    console.log("----------------------");
+    console.log("Found Template, Updating and Publishing...");
+    console.log("----------------------");
+    console.log("\n");
+    const updateResponse = await updateAndPublishTemplate({ name, content });
+    console.log(updateResponse);
+  } else {
+    // Template Does not exist
+    console.log("----------------------");
+    console.log("Template Not Found, Adding New Template...");
+    console.log("----------------------");
+    console.log("\n");
+    const updateResponse = await addAndPublishTemplate({ name, content });
+    console.log(updateResponse);
+  }
+};
+
+const readFileContents = () => {
+  const fileNames = JSON.parse(core.getInput("fileNames"));
+  fileNames.forEach((fileName) => {
+    const fullPath = path.resolve(fileName);
+    core.info(`Processing file: ${fullPath}`);
+    const breaks = fileName.split("/");
+    const exactFileName = breaks[breaks.length - 1];
+    const rawdata = fs.readFileSync(fullPath);
+    console.log(exactFileName);
+    console.log(rawdata);
+    console.log("\n");
+    addOrUpdateTemplate({ name: exactFileName, content: rawdata });
+  });
+};
 
 async function callPing() {
   validatePrimaryInputs();
@@ -53,27 +92,7 @@ async function callPing() {
   console.log(response);
   console.log("----------------------");
   console.log("\n");
-  console.log("----------------------");
-  console.log("Check for template");
-  console.log("----------------------");
-  console.log("\n");
-  const { status: getTemplateStatus } = findTemplate();
-
-  if(getTemplateStatus === 200) { // Template Found
-    console.log("----------------------");
-    console.log("Found Template, Updating and Publishing...");
-    console.log("----------------------");
-    console.log("\n");
-    const updateResponse = await updateAndPublishTemplate();
-    console.log(updateResponse);
-  } else { // Template Does not exist
-    console.log("----------------------");
-    console.log("Template Not Found, Adding New Template...");
-    console.log("----------------------");
-    console.log("\n");
-    const updateResponse = await addAndPublishTemplate();
-    console.log(updateResponse);
-  }
+  readFileContents();
 }
 
 callPing(); // if mandrill works, it will return "PONG!"
